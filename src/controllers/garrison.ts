cbc661ec3dd097721a7598028140c27deae8a144
Scott Williams <scott.joe.williams@gmail.com>
2021-08-25 22:24:13 -0400

    update

--- src/controllers/garrison.ts
+++ src/controllers/garrison.ts
@@ -16,31 +16,31 @@ const spawnCreep = (spawn: StructureSpawn, role: CreepRole): void => {
 
 export default {
     run: (spawn: StructureSpawn): void => {
-        // if not actively spawning
         if (!spawn.spawning) {
             // TODO: spawn.renewCreep
             if (spawn.store.energy >= minEnergy) {
-                // Check if creeps are needed
                 switch (true) {
                     case shouldSpawn(CreepRole.HARVESTER):
+                        console.log(`Spawning ${CreepRole.HARVESTER}`)
                         spawnCreep(spawn, CreepRole.HARVESTER)
                         break
                     case shouldSpawn(CreepRole.UPGRADER):
+                        console.log(`Spawning ${CreepRole.UPGRADER}`)
                         spawnCreep(spawn, CreepRole.UPGRADER)
                         break
                     case shouldSpawn(CreepRole.BUILDER):
+                        console.log(`Spawning ${CreepRole.BUILDER}`)
                         spawnCreep(spawn, CreepRole.BUILDER)
                         break
-                    case shouldSpawn(CreepRole.SOLDIER):
-                        spawnCreep(spawn, CreepRole.SOLDIER)
-                        break
+                    // case shouldSpawn(CreepRole.SOLDIER):
+                    //     spawnCreep(spawn, CreepRole.SOLDIER)
+                    //     break
                     default:
                         break
                 }
             }
         } else {
             const newCreepName = spawn.spawning.name
-            const newCreep = Game.creeps[newCreepName]
             spawn.room.visual.text(
                 `🛠️ ${newCreepName}`,
                 spawn.pos.x + 1,
