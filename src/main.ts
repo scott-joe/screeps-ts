5c5ffd8838a5928acf1f2e12bca9cb59e875291d
Scott Williams <scott.joe.williams@gmail.com>
2021-09-01 14:55:53 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,5 @@
 import { ErrorMapper } from 'utils/ErrorMapper'
-// require('extensions/index')
-import { CreepRole, Division } from 'types/main'
 import Base from 'controllers/Base'
-
-declare global {
-    /*
-      Example types, expand on these or remove them and add your own.
-      Note: Values, properties defined here do not fully *exist* by this type definiton alone.
-            You must also give them an implemention if you would like to use them. (ex. actually setting a `role` property in a Creeps memory)
-
-      Types added in this `global` block are in an ambient, global context. This is needed because `main.ts` is a module file (uses import or export).
-      Interfaces matching on name from @types/screeps will be merged. This is how you can extend the 'built-in' interfaces from @types/screeps.
-    */
-    // Memory extension samples
-    interface Memory {
-        uuid: number
-        log: any
-    }
-
-    interface CreepMemory {
-        room?: string
-        role: CreepRole
-        division?: Division
-        working?: boolean
-        building?: boolean
-        upgrading?: boolean
-    }
-
-    interface Creep {
-        renew: Function
-    }
-
-    interface RoomMemory {
-        census: { [x: string]: { min: number; cur: number } }
-        spawnQueue: CreepRole[],
-        creepId: number
-    }
-
-    interface Room {
-        base: Function
-        size: number
-    }
-
-    // Syntax for adding proprties to `global` (ex "global.log")
-    // eslint-disable-next-line @typescript-eslint/no-namespace
-    namespace NodeJS {
-        interface Global {
-            log: any
-        }
-    }
-}
 
 export const loop = ErrorMapper.wrapLoop(() => {
     for (const id in Game.rooms) {
--- src/types/main.ts
+++ src/types/main.ts
@@ -10,7 +10,7 @@ export enum CreepRole {
 }
 
 export interface Census {
-	[x: string]: CensusStatus
+    [x: string]: CensusStatus
 }
 
 export enum Division {
@@ -36,5 +36,52 @@ export enum Size {
 export type CreepRecipe = {
     [property in CreepRole]: {
         [property in Size]: Array<BodyPartConstant>
+    }
+}
+
+declare global {
+    /*
+      Example types, expand on these or remove them and add your own.
+      Note: Values, properties defined here do not fully *exist* by this type definiton alone.
+            You must also give them an implemention if you would like to use them. (ex. actually setting a `role` property in a Creeps memory)
+
+      Types added in this `global` block are in an ambient, global context. This is needed because `main.ts` is a module file (uses import or export).
+      Interfaces matching on name from @types/screeps will be merged. This is how you can extend the 'built-in' interfaces from @types/screeps.
+    */
+    // Memory extension samples
+    interface Memory {
+        uuid: number
+        log: any
+    }
+
+    interface CreepMemory {
+        room?: string
+        role: CreepRole
+        division?: Division
+        working?: boolean
+        building?: boolean
+        upgrading?: boolean
+    }
+
+    interface Creep {
+        renew: Function
+    }
+
+    interface RoomMemory {
+        census: { [x: string]: { min: number; cur: number } }
+        spawnQueue: CreepRole[]
+        creepId: number
+    }
+
+    interface Room {
+        base: Function
+        size: number
+    }
+
+    // Syntax for adding proprties to `global` (ex "global.log")
+    namespace NodeJS {
+        interface Global {
+            log: any
+        }
     }
 }
